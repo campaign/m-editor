d3811349b6f4fb094a797fe4aeb3a3fce075eb68
campaign <zhanyi1022@gmail.com>
2013-11-12 01:22:27 +0800

    创建项目

--- editor_api.js
+++ editor_api.js
@@ -17,7 +17,7 @@
             'core/htmlparser.js',
             'core/filternode.js'
         ],
-        baseURL = '/_src/';
+        baseURL = 'src/';
     for (var i=0,pi;pi = paths[i++];) {
         document.write('<script type="text/javascript" src="'+ baseURL + pi +'"></script>');
     }
