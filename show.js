635272de1f3d6be83d891d711fcc3a249d5a3b3c
Jinqn <459452372>
2013-12-04 15:01:42 +0800

    by Jinqn

--- show.js
+++ show.js
@@ -5,11 +5,20 @@
     Slider.prototype = {
         init:function(){
             var _this = this;
-            this.$root = $('<div class="slider_container">').hide();
+            this.$root = $('<div class="slider_container">')
+                .css({
+                    'background': '##666666',
+                    'position': 'fixed',
+                    'top': 0,
+                    'left': 0,
+                    'width': $(window).width(),
+                    'height': $(window).height()
+                }).hide();
             this.$root.appendTo(document.body);
             this.$root.on('tap', function(){
                 _this.hide();
             });
+            return this;
         },
         setImages: function($imgs){
             var $root = this.$root;
@@ -17,12 +26,26 @@
             $imgs.each(function(k, img){
                 $('<div><img src="'+ $(img).attr('src') + '" /></div>').appendTo($root);
             });
+            return this;
         },
-        show: function(){
-            this.$root.show().slider( { imgZoom: true });
+        show: function(slideToIndex){
+            var $root = this.$root;
+            $root.show().slider({
+                index: slideToIndex || 0,
+                ready: function () {
+                    $root.find('img').on('load', updataImagePos).each(updataImagePos);
+                    function updataImagePos(){
+                        var $img = $(this),
+                            marginTop = ($img.parent().height() - $img.height()) / 2;
+                        $img.css({'margin': (marginTop > 0 ? marginTop : 0) + 'px auto'});
+                    }
+                }
+            });
+            return this;
         },
         hide: function(){
             this.$root.hide().slider('destroy');
+            return this;
         }
     };
 
@@ -30,7 +53,7 @@
     $('.item').each(function(i, p){
         var $imgs = $(p).find('.post img');
         $imgs.click(function(){
-            slider.setImages($imgs);
+            slider.setImages($imgs).show(i);
         });
     });
 })()
