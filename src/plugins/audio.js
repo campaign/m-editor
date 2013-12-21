9644f2ee866480ced9e07dc4ab96404ce0a46550
Jinqn <459452372@qq.com>
2013-12-21 16:19:54 +0800

    by Jinqn 语音点击播放

--- src/plugins/audio.js
+++ src/plugins/audio.js
@@ -1,14 +1,14 @@
 UM.plugins.audio = function(){
     var me = this;
 
-//    me.addInputRule(function(root){
-//        utils.each(root.getNodesByTagName('audio'), function(node){
-//            var src = node.getAttr('src');
-//            node.tagName = 'img';
-//            node.setAttr('_src', src);
-//            node.setAttr('src', 'themes/images/holder.png');
-//            node.setAttr('class', 'audio');
-//        });
-//    });
+    me.addInputRule(function(root){
+        utils.each(root.getNodesByTagName('audio'), function(node){
+            var src = node.getAttr('src');
+            node.tagName = 'img';
+            node.setAttr('_src', src);
+            node.setAttr('src', 'themes/images/holder.png');
+            node.setAttr('class', 'audio');
+        });
+    });
 
 };
